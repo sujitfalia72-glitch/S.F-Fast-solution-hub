ed8406dae1b062fe347cb62f1d8f3f16bb886638
sujitfalia72-glitch <sujitfalia72@gmail.com>
2026-06-27 23:40:32 +0530

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from .chat import Chat
+from .user import User
+from .booking import Booking
+from models.profile import Profile
+from models.work_model import Work
+
+from .doctor.doctor import Doctor
+from .doctor.schedule import DoctorSchedule
+from .appointment import Appointment
+from .chamber_profile import ChamberProfile
+from .site_setting import SiteSetting
